3da5e3afcb1ab9793b2cf2d7f043badd0bb3db79
Salil Trehan <trehansalil1@gmail.com>
2024-10-08 13:43:20 +0000

    Refactored code for security

--- medspa_ai/constants/__init__.py
+++ medspa_ai/constants/__init__.py
@@ -1,13 +1,19 @@
 import configparser
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 config_path = os.path.join(os.getcwd(), "config_file.config")
 
 config_parser = configparser.ConfigParser()
 config_parser.read(config_path)
 
-mongo_db_uri = config_parser.get('mongo_config', 'mongo_db_uri')
+# Fetch the value of mongo_db_uri from .env
+mongo_db_uri = os.getenv('MONGO_DB_URI')
+# mongo_db_uri = config_parser.get('mongo_config', 'mongo_db_uri')
 mongo_db_name = config_parser.get('mongo_config', 'mongo_db_name')
 mongo_db_coll_input_diagnosis = config_parser.get('mongo_config', 'mongo_db_coll_input_diagnosis')
 
